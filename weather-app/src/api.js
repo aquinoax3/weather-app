538e289d2c63045886746f7838c23df565319a93
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2022-10-18 01:59:39 -0400

    Search bar for cities

---
+++ weather-app/src/api.js
@@ -0,0 +1,10 @@
+export  const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e7f3560facmsh187a7449982a5e2p1dad0cjsn3b06b626f683',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "'https://wft-geo-db.p.rapidapi.com/v1/geo"
+
---
+++ weather-app/src/components/search/search.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../api.js"
+
+
+
+
+const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch] = useState(null)
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, 
+            options
+        ) 
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err)); 
+    }
+
+const handleOnChange = (searchData) => {
+    setSearch(searchData)
+    onSearchChange(searchData)
+}
+
+
+return (
+    <AsyncPaginate
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+    />
+
+)
+}
+
+export default Search
